9ed3874448df504663a8110bbd0645c987ce6f0f
yanling-99 <love87090959@gmail.com>
2024-08-04 14:44:24 +0800

    init

---
+++ menu/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+const items = ref([
+  { item: "珍珠奶茶", desc: "香濃奶茶搭配QQ珍珠", price: 50, count: 20 },
+  { item: "冬瓜檸檬", desc: "清新冬瓜配上新鮮檸檬", price: 45, count: 18 },
+  { item: "珍珠奶茶", desc: "香濃奶茶搭配QQ珍珠", price: 50, count: 20 },
+  { item: "冬瓜檸檬", desc: "清新冬瓜配上新鮮檸檬", price: 45, count: 18 },
+]);
+
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th scope="col">品項</th>
+        <th scope="col">描述</th>
+        <th scope="col">價格</th>
+        <th scope="col">庫存</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td>{{item.item}}</td>
+        <td><small>{{ item.desc }}</small></td>
+        <td>{{ item.price }}</td>
+        <td><button >-</button>{{ item.count }}<button>+</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
