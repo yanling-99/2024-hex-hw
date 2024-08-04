393d52f00a14ab9b36ed295c3e75f178767bbd89
yanling-99 <love87090959@gmail.com>
2024-08-04 22:33:34 +0800

    deploy setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import { url } from 'inspector';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2024-hex-hw/',
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+});
